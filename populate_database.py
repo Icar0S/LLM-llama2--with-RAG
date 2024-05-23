77827e3fcf6a50ec843b9f08e837bf67c9e5f6fa
Wilson Castro <wilsoncastro.pv@gmail.com>
2024-05-23 16:46:50 -0300

    fix

--- populate_database.py
+++ populate_database.py
@@ -1,12 +1,11 @@
 import argparse
 import os
 import shutil
-from langchain.document_loaders.pdf import PyPDFDirectoryLoader
+from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.schema.document import Document
-from get_embedding_function import get_embedding_function
-from langchain.vectorstores.chroma import Chroma
-
+from get_embedding import get_embedding_function
+from langchain_community.vectorstores.chroma import Chroma # from langchain.vectorstores import Chroma se der erro
 
 CHROMA_PATH = "chroma"
 DATA_PATH = "data"
