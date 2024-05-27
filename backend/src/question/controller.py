4770375d5bcaf1381cc84ebd5dc3d9a95d136449
Wilson Castro <wilsoncastro.pv@gmail.com>
2024-05-27 10:05:08 -0300

    fix: adjust request question capture

--- backend/src/question/controller.py
+++ backend/src/question/controller.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
 from src.question.service import query_rag 
-
+# 
 question_router = APIRouter(prefix="/question", tags=["Question"])
 
-@question_router.get("/{question}")
-def response_question(question: str):
+@question_router.post("/ask", summary="Ask a question to the RAG model")
+def response_question(req: dict):
+    question = req.get("question", None)
     return {"question": question,  **query_rag(question)}
